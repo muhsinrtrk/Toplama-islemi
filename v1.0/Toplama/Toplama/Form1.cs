583fe61854d875c801a406d7611441ccab423c37
muhsinrtrk <48183405+muhsinrtrk@users.noreply.github.com>
2019-09-22 20:20:50 +0300

    Add files via upload

---
+++ v1.0/Toplama/Toplama/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Toplama
+{
+    public partial class frmToplama : Form
+    {
+        public frmToplama()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToplama_Click(object sender, EventArgs e)
+        {
+            int birincideger, ikincideger, toplamdeger;
+
+            birincideger = Convert.ToInt32(txtBirinciSayi.Text);
+            ikincideger = Convert.ToInt32(txtIkinciSayi.Text);
+            toplamdeger = birincideger + ikincideger;
+            MessageBox.Show("Sonuç: " + toplamdeger);
+        }
+    }
+}
